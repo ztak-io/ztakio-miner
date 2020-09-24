2c6fc1d1259b9f43bab3436483e3ac17d0b0ec76
chiguireitor <johnvillarzavatti@gmail.com>
2020-09-24 02:04:53 -0400

    Multiple fixes to prevent certain race conditions (still remaining some)

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ function start() {
       } else if (ob.method === 'event') {
         // It's a subscription
         events.emit('event', ob.params)
-        console.log('Subscription>', ob)
+        //console.log('Subscription>', ob)
       }
     }
   })
@@ -78,82 +78,94 @@ function start() {
     }
   }, Math.floor(CB_TIMEOUT / 4)).unref()
 
+  let mining = false
   const mine = async (mempool) => {
-    console.log('Mining a block')
-    const ecpair = bitcoin.ECPair.fromWIF(WIF)
-    let network = NETWORK
+    if (mining) return
+    mining = true
+    try {
+      console.log('Mining a block')
+      const ecpair = bitcoin.ECPair.fromWIF(WIF)
+      let network = NETWORK
 
-    const { address } = bitcoin.payments.p2pkh({ pubkey: ecpair.publicKey, network: network })
+      const { address } = bitcoin.payments.p2pkh({ pubkey: ecpair.publicKey, network: network })
 
-    if (!mempool) {
-      mempool = await send('core.mempool', [])
-    }
-    let poaSignedTxs = {}
+      if (!mempool) {
+        mempool = await send('core.mempool', [])
+      }
+      let poaSignedTxs = {}
 
-    console.log(`Got ${mempool.length} transactions from the mempool, our address is ${address}`)
-    for (let i=0; i < mempool.length; i++) {
-      let txid = mempool[i]
-      let txFeds = await send('core.get', [`/_/tx.${txid}.feds`])
+      console.log(`Got ${mempool.length} transactions from the mempool, our address is ${address}`)
+      let p = 0
+      for (let i=0; i < mempool.length; i++) {
+        if (p > 100) {
+          break
+        }
+        p++
+        let txid = mempool[i]
+        let txFeds = await send('core.get', [`/_/tx.${txid}.feds`])
 
-      for (let fed in txFeds) {
-        let fedData = await send('core.get', [`${fed}.meta`])
-        if (fedData.FedType === 'poa') {
-          // This federation uses proof of authority
-          for (let poaIdx=0; poaIdx < 10000; poaIdx++) {
-            let key = `${fed}/_poa_${poaIdx}`
-            let poaData = await send('core.get', [key])
+        for (let fed in txFeds) {
+          let fedData = await send('core.get', [`${fed}.meta`])
+          if (fedData.FedType === 'poa' && typeof(txFeds[fed]) === 'object') {
+            // This federation uses proof of authority
+            for (let poaIdx=0; poaIdx < 10000; poaIdx++) {
+              let key = `${fed}/_poa_${poaIdx}`
+              let poaData = await send('core.get', [key])
 
-            if (poaData !== null) {
-              if (poaData === address) {
-                if (!(fed in poaSignedTxs)) {
-                  poaSignedTxs[fed] = {}
+              if (poaData !== null) {
+                if (poaData === address) {
+                  if (!(fed in poaSignedTxs)) {
+                    poaSignedTxs[fed] = {}
+                  }
+                  // We can sign this!
+                  //let signature = ecpair.sign(Buffer.from(txid, 'hex'))
+                  let signature = bitcoinMessage.sign(txid, ecpair.privateKey, ecpair.compressed)
+                  poaSignedTxs[fed][txid] = [poaIdx, signature.toString('base64')]
+                  console.log(`${i + 1}/${mempool.length} Included ${txid} from federation ${fed} on the new block`)
                 }
-                // We can sign this!
-                //let signature = ecpair.sign(Buffer.from(txid, 'hex'))
-                let signature = bitcoinMessage.sign(txid, ecpair.privateKey, ecpair.compressed)
-                poaSignedTxs[fed][txid] = [poaIdx, signature.toString('base64')]
-                console.log(`${i + 1}/${mempool.length} Included ${txid} from federation ${fed} on the new block`)
+              } else {
+                // On the first "null" we bailout
+                break
               }
-            } else {
-              // On the first "null" we bailout
-              break
             }
           }
         }
       }
-    }
 
-    if (Object.keys(poaSignedTxs).length > 0) {
-      let opcodes = []
-      for (let fed in poaSignedTxs) {
-        opcodes.push(`REQUIRE ${fed}`)
-        let txs = Object.entries(poaSignedTxs[fed]).sort((a, b) => a[0].localeCompare(b[0]))
-        for (let txIdx=0; txIdx < txs.length; txIdx++) {
-          let tx = txs[txIdx]
-          opcodes.push(`PUSHS "${tx[0]}"`)
-          opcodes.push(`PUSHI ${tx[1][0]}`)
-          opcodes.push(`PUSHS "${tx[1][1]}"`)
-          opcodes.push(`ECALL ${fed}:federation`)
+      if (Object.keys(poaSignedTxs).length > 0) {
+        let opcodes = []
+        for (let fed in poaSignedTxs) {
+          opcodes.push(`REQUIRE ${fed}`)
+          let txs = Object.entries(poaSignedTxs[fed]).sort((a, b) => a[0].localeCompare(b[0]))
+          for (let txIdx=0; txIdx < txs.length; txIdx++) {
+            let tx = txs[txIdx]
+            opcodes.push(`PUSHS "${tx[0]}"`)
+            opcodes.push(`PUSHI ${tx[1][0]}`)
+            opcodes.push(`PUSHS "${tx[1][1]}"`)
+            opcodes.push(`ECALL ${fed}:federation`)
+          }
+        }
+        opcodes.push('PUSHI 1')
+        opcodes.push('VERIFY "tx-error-while-verify"')
+        opcodes.push(`END`)
+        let code = opcodes.join('\n')
+        let byteCode = ztak.asm.compile(code)
+        let hex = ztak.buildEnvelope(ecpair, byteCode).toString('hex')
+        //console.log(hex)
+        console.log(`Submitting new block with ${Object.keys(poaSignedTxs).length} transactions, ${hex.length} bytes`)
+
+        for (let i=0; i < 5; i++) {
+          try {
+            await send('core.block', [hex])
+            console.log('Block submitted')
+            break
+          } catch(e) {
+            console.log(`(${i+1}/5 tries) Error while submitting block:`, e)
+          }
         }
       }
-      opcodes.push('PUSHI 1')
-      opcodes.push('VERIFY "tx-error-while-verify"')
-      opcodes.push(`END`)
-      let code = opcodes.join('\n')
-      let byteCode = ztak.asm.compile(code)
-      let hex = ztak.buildEnvelope(ecpair, byteCode).toString('hex')
-      console.log(hex)
-      console.log(`Submitting new block with ${Object.keys(poaSignedTxs).length} transactions, ${hex.length} bytes`)
-
-      for (let i=0; i < 5; i++) {
-        try {
-          await send('core.block', [hex])
-          console.log('Block submitted')
-          break
-        } catch(e) {
-          console.log(`(${i+1}/5 tries) Error while submitting block:`, e)
-        }
-      }
+    } finally {
+      mining = false
     }
   }
 
@@ -170,21 +182,31 @@ function start() {
     })
 
     let mineTimeout
-    events.on('event', async (keys) => {
-      if (keys.filter(x => x === '/_/mempool').length > 0) {
+    let waitPeriod = 1500
+    const doBlock = async () => {
+      await mine()
+      mineTimeout = null
+      waitPeriod = 1500
+    }
+    let n = 0
+    const tryMine = async (keys) => {
+      if (keys && keys.filter(x => x === '/_/mempool').length > 0) {
         if (!mineTimeout) {
-          mineTimeout = setTimeout(async () => {
-            await mine()
-            mineTimeout = null
-          }, 1500)
+          mineTimeout = setTimeout(doBlock, 1500)
+        } else if (waitPeriod > 100){
+          waitPeriod = Math.ceil(waitPeriod * 0.95)
+          clearTimeout(mineTimeout)
+          mineTimeout = setTimeout(doBlock, waitPeriod)
         }
       }
-    })
+    }
+
+    events.on('event', tryMine)
 
     const checkMempool = async () => {
       let mempool = await send('core.get', ['/_/mempool'])
       if (mempool && mempool.length > 0) {
-        await mine(mempool)
+        //await mine(mempool)
       }
     }
     checkMempool()
